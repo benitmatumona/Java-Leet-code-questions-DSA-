4eec865ce3d018f58b84c086f4d18c6e5614954c
benit <polviedivine@gmail.com>
2026-05-02 18:23:00 +0200

    solotion attempt

--- Arrays.java
+++ Arrays.java
@@ -1,10 +1,11 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Arrays{
 
     public static void main(String[] args) {
         int[] test = {1, 7, 3, 2};
-        System.out.println(twoSum(test, 9)[0] + " " + twoSum(test, 9)[1]);
+        System.out.println(containsDuplicates(new int[] {1, 2, 3, 4, 5, 6, 7}));
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -15,6 +16,15 @@ public class Arrays{
             else  map.put(nums[i], i);
         }
         return new int[] {};
+    }
+
+    public static boolean containsDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>(); 
+        for (int num: nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        } 
+        return false;
     }
     
 }
